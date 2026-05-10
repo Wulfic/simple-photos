e5fc54891d3c395623384246e5cdc8fa6c567039
Wulfic <Wulfic@users.noreply.github.com>
2026-05-10 04:22:35 -0400

    chore(release): bump version to 1.0.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.simplephotos"
         minSdk = 26
         targetSdk = 34
-        versionCode = 102
-        versionName = "1.0.2"
+        versionCode = 103
+        versionName = "1.0.3"
         testInstrumentationRunner = "com.simplephotos.HiltTestRunner"
     }
 
