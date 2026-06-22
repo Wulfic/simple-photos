406ad9e12509798b414e80e4d2d2578e9f648d5d
Wulfic <Wulfic@users.noreply.github.com>
2026-06-22 04:06:09 -0400

    chore(release): bump version to 1.4.90

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.simplephotos"
         minSdk = 26
         targetSdk = 34
-        versionCode = 128
-        versionName = "1.3.73"
+        versionCode = 129
+        versionName = "1.4.90"
         testInstrumentationRunner = "com.simplephotos.HiltTestRunner"
     }
 
